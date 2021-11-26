9df09f50dc093fc24c20d2daa3e14e873e2d03ec
Pedro Caldeira <pedromigcc@gmail.com>
2021-11-26 13:29:30 +0000

    feat: middleware for auth handling

--- tickets/auth/src/routes/signin.ts
+++ tickets/auth/src/routes/signin.ts
@@ -1,7 +1,48 @@
-import express from 'express';
-
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email mush be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You mush supply a password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email: email });
+    if (!existingUser) {
+      throw new BadRequestError('Failed to Authenticate.');
+    }
+    const isPasswordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!isPasswordMatch) {
+      throw new BadRequestError('Failed to Authenticate.');
+    }
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // Store it in session object
+    req.session = { jwt: userJwt };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
