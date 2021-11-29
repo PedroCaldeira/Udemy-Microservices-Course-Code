50c24f6c3f9e36f9341da26f8f0991d2d0658782
Pedro Caldeira <pedromigcc@gmail.com>
2021-11-29 17:13:45 +0000

    fix: use envvar for mongodb url

--- tickets/auth/src/index.ts
+++ tickets/auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY is not defined.');
   }
+  if (!process.env.MONGO_DB_URI) {
+    throw new Error('MONGO_DB_URI is not defined.');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log('Connected to MongoDb');
   } catch (error) {
     console.error(error);
